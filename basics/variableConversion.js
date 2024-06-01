3c397eaf4e3a827b8e9e2b3019ab11a30be12c47
Shreyash Meshram <91451934+shreyuu@users.noreply.github.com>
2024-06-01 06:06:06 +0530

    variables

---
+++ basics/variableConversion.js
@@ -0,0 +1,17 @@
+let score = "38"
+console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof(score))
+console.log(typeof(valueInNumber))
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(isLoggedIn);
+
+let num = 23
+let numToString = String(num)
+console.log(typeof num);
+console.log(typeof numToString);
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 1001
+let accountEmail = "shreyash@gmail.com"
+var accountPassword = "shreyash@123"
+accountCity = "Nashik"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
